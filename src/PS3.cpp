7dbab76d13149949d03034bf5d15ab263166d12b
Omid Karimpour <okarimpour@gmail.com>
2019-08-25 12:16:38 -0400

    Refactored PS3.cpp

--- src/PS3.cpp
+++ src/PS3.cpp
@@ -6,42 +6,88 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
-using namespace cv;
-using namespace std;
+inline bool failedToLoad(cv::Mat image) {
+    if(image.empty() || 0 == image.size().width || 0 == image.size().height){
+        return true;
+    }
+    return false;
+}
+
+cv::Mat readImage(std::string fileName){
+    cv::Mat image = cv::imread(fileName);
+    if (failedToLoad(image))
+        throw "Image not found";
+    return image;
+}
+
+void writeImage(std::string fileName, cv::Mat newImage){
+    cv::imwrite(fileName, newImage);
+}
+
+void showImage(cv::Mat newFrontImage, std::string label){
+    cv::namedWindow(label, cv::WINDOW_AUTOSIZE);
+    cv::imshow(label, newFrontImage);
+    cv::waitKey();
+    cv::destroyAllWindows();
+}
 
 int main (int argc, const char * argv[])
 {
+    cv::Mat mainImage;
+    cv::Mat layeredImage;
+    static_cast<void>(argc);
+    static_cast<void>(argv);
     
-    //Capture the image
-    Mat mainImage = imread("/Users/omidkarimpour/Desktop/ComputerVision/secondClass/input/mainImage.jpg");
-    Mat layeredImage = imread("/Users/omidkarimpour/Desktop/ComputerVision/secondClass/input/layeredImage.jpg", IMREAD_UNCHANGED);
-    if (mainImage.data == 0 || layeredImage.data == 0) {
-        cerr << "Image not found!" << endl;// Show image not found if there is no data
+    //Reading the image
+    try{
+        mainImage = readImage("/Users/omidkarimpour/Desktop/ComputerVision/secondClass/input/mainImage.jpg");
+        layeredImage = readImage("/Users/omidkarimpour/Desktop/ComputerVision/secondClass/input/layeredImage.jpg");
+    }
+    catch(const cv::Exception & e){
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
+    catch (const char* error){
+        std::cerr << error << std::endl;
+        return -1;
+    }
+
+    //Resizing the front image.
+    std::cout << "Main image size:\nWidth : " << mainImage.size().width << "\n" << "Height: " << mainImage.size().height << "\nLayered image size:" << "\nWidth : " << layeredImage.size().width << "\nHeight: " << layeredImage.size().height << std::endl;
+
+    cv::Mat newLayeredImage;
+    
+    cv::resize(layeredImage, newLayeredImage, mainImage.size());
+    
+    //Writing the image
+    try{
+        writeImage("/Users/omidkarimpour/Desktop/ComputerVision/secondClass/output/new_layeredImage.jpg", newLayeredImage);
+    }
+    catch(const cv::Exception & e){
+        std::cerr << e.what() << std::endl;
         return -1;
     }
     
-    //get the sizes
-    cout << "Main image size:" << endl;
-    cout << "Width : " << mainImage.size().width << endl;
-    cout << "Height: " << mainImage.size().height << endl;
-    cout << "Layered image size:" << endl;
-    cout << "Width : " << layeredImage.size().width << endl;
-    cout << "Height: " << layeredImage.size().height << endl;
-    
-    //resize
-    Mat new_layeredImage;
-    resize(layeredImage, new_layeredImage, Size(mainImage.size().width, mainImage.size().height));
-    imwrite("/Users/omidkarimpour/Desktop/ComputerVision/secondClass/output/new_layeredImage.jpg", new_layeredImage);
-    namedWindow("new Layered", WINDOW_AUTOSIZE);
-    imshow("new Layered", new_layeredImage);
-    
+    //Showing the image
+    showImage(newLayeredImage, "newLayeredImage");
+
     //overlayer
-    Mat finalImage;
+    cv::Mat finalImage;
     double alpha = 0.8;
     double beta = 1 - alpha;
-    addWeighted(mainImage, alpha, new_layeredImage, beta, 0.0,finalImage);
-    imwrite("/Users/omidkarimpour/Desktop/ComputerVision/secondClass/output/finalImage.jpg", finalImage);
-    namedWindow("final Image", WINDOW_AUTOSIZE);
-    imshow("final Image", finalImage);
+    cv::addWeighted(mainImage, alpha, newLayeredImage, beta, 0.0,finalImage);
     
+    //Writing the result
+    try{
+        writeImage("/Users/omidkarimpour/Desktop/ComputerVision/secondClass/output/finalImage.jpg", finalImage);
+    }
+    catch(const cv::Exception & e){
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
+    
+    //Writing the result
+    showImage(finalImage, "finalImage");
+    
+    return 0;
 }
