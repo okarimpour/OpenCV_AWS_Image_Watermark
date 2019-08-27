02c7e8e3a7abcf140ab181a210cae05d75e3c9f6
Omid Karimpour <okarimpour@gmail.com>
2019-08-27 18:20:34 -0400

    Wrote the overlayImage function and modify the resizing function

--- src/PS3.cpp
+++ src/PS3.cpp
@@ -20,8 +20,8 @@ cv::Mat readImage(std::string fileName){
     return image;
 }
 
-void resizingImage(cv::Mat originalImage, cv::Mat &resizedImage, cv::Mat backgroundImage, double xFactor, double yFactor){
-    cv::resize(originalImage, resizedImage, cv::Size(backgroundImage.cols*xFactor, backgroundImage.rows*yFactor), 0, 0, cv::INTER_AREA);
+void resizingImage(cv::Mat originalImage, cv::Mat &resizedImage, cv::Mat backgroundImage, double Factor){
+    cv::resize(originalImage, resizedImage, cv::Size(backgroundImage.cols*Factor, backgroundImage.cols*Factor* (originalImage.rows/originalImage.cols)), 0, 0, cv::INTER_AREA);
     if (failedToLoad(resizedImage))
         throw "Could not resize";
 }
@@ -73,9 +73,36 @@ void printSize(std::string name, cv::Mat image){
     std::cout << name << " image size:\nWidth : " << image.size().width << "\nHeight: " << image.size().height << std::endl;
 }
 
+void overlayImage(cv::Mat &background, cv::Mat &foreground, cv::Mat &finalImage, cv::Point2i location){
+    
+    cv::Mat rgba[4];
+    background.copyTo(finalImage);
+    
+    for(int rows = std::max(location.y, 0); rows < background.rows; ++rows)
+    {
+        int foregroundY = rows - location.y;
+        
+        if(foregroundY >= foreground.rows)
+            break;
+        
+        for(int cols = std::max(location.x, 0); cols < background.cols; ++cols){
+            int foregroundX = cols - location.x;
+            
+            if(foregroundX >= foreground.cols)
+                break;
+            
+            if(foreground.at<cv::Vec4b>(foregroundY, foregroundX)[3] == 255 && foreground.at<cv::Vec4b>(foregroundY, foregroundX)[0] != 255 && foreground.at<cv::Vec4b>(foregroundY, foregroundX)[1] != 255 && foreground.at<cv::Vec4b>(foregroundY, foregroundX)[2] != 255){
+                finalImage.at<cv::Vec3b>(rows, cols)[0] = foreground.at<cv::Vec4b>(foregroundY, foregroundX)[0];
+                finalImage.at<cv::Vec3b>(rows, cols)[1] = foreground.at<cv::Vec4b>(foregroundY, foregroundX)[1];
+                finalImage.at<cv::Vec3b>(rows, cols)[2] = foreground.at<cv::Vec4b>(foregroundY, foregroundX)[2];
+            }
+        }
+    }
+}
+
 int main (int argc, const char * argv[])
 {
-    cv::Mat mainImage, layeredImage, newLayeredImage, newMainImage;
+    cv::Mat mainImage, layeredImage, newLayeredImage, newMainImage, finalImage;
     static_cast<void>(argc);
     static_cast<void>(argv);
     
@@ -85,33 +112,37 @@ int main (int argc, const char * argv[])
     printSize("main", mainImage);
     printSize("layered", layeredImage);
     
-//    cv::split(layeredImage, bgra);
-//    std::vector<cv::Mat> channels = {bgra[0], bgra[1], bgra[2]};
-//    cv::merge(channels, rgbImage);
-//    showImage(rgbImage, "layeredImage");
-    
-//    //overlayer
-//    cv::Mat finalImage;
-//    double alpha = 0.5;
-//    double beta = 1 - alpha;
-//    cv::addWeighted(newMainImage, alpha, newLayeredImage, beta, 0.0,finalImage);
-    
     cv::cvtColor(mainImage, newMainImage, cv::COLOR_RGB2RGBA, 4);
     
     if(layeredImage.size >= newMainImage.size){
-        
-        resizingImage(layeredImage, newLayeredImage, newMainImage, 0.1, 0.1);
-        newLayeredImage.copyTo(newMainImage(cv::Rect(0, 0, newLayeredImage.cols, newLayeredImage.rows)));
-        
+        resizingImage(layeredImage, newLayeredImage, newMainImage, 0.2);
+        overlayImage(newMainImage, newLayeredImage, finalImage, cv::Point(0, 0));
     } else{
-        //upsample
+        //upsample(Lateron)
+        resizingImage(layeredImage, newLayeredImage, newMainImage, 0.2);
+        overlayImage(newMainImage, newLayeredImage, finalImage, cv::Point(0, 0));
     }
     
-        printSize("new layered", newLayeredImage);
-        tryCatch("/Users/omidkarimpour/Desktop/openCV_Project/output/newMainImage.png", newMainImage, writeImage);
-        tryCatch("/Users/omidkarimpour/Desktop/openCV_Project/output/newLayeredImage.png", newLayeredImage, writeImage);
-        showImage(newMainImage, "newMainImage");
-        showImage(newLayeredImage, "newLayeredImage");
+    printSize("new layered", newLayeredImage);
+    tryCatch("/Users/omidkarimpour/Desktop/openCV_Project/output/newLayeredImage.png", newLayeredImage, writeImage);
+    tryCatch("/Users/omidkarimpour/Desktop/openCV_Project/output/finalImage.png", finalImage, writeImage);
+    showImage(newLayeredImage, "newLayeredImage");
+    showImage(finalImage, "finalImage");
         
     return 0;
 }
+
+        
+        
+        //    cv::split(layeredImage, bgra);
+        //    std::vector<cv::Mat> channels = {bgra[0], bgra[1], bgra[2]};
+        //    cv::merge(channels, rgbImage);
+        //    showImage(rgbImage, "layeredImage");
+        //    //overlayer
+        //    cv::Mat finalImage;
+        //    double alpha = 0.5;
+        //    double beta = 1 - alpha;
+        //    cv::addWeighted(newMainImage, alpha, newLayeredImage, beta, 0.0,finalImage);
+        //    cv::cvtColor(mainImage, newMainImage, cv::COLOR_RGB2RGBA, 4);
+        //    resizingImage(layeredImage, newLayeredImage, newMainImage, 0.1, 0.1);
+        //    newLayeredImage.copyTo(newMainImage(cv::Rect(0, 0, newLayeredImage.cols, newLayeredImage.rows)));
